c0d15a86c05a0de73e8ce4a56928f88cc0426e75
Filip Haužvic <hauzvicfilip@gmail.com>
2022-04-12 11:00:21 +0200

    Added private header file

---
+++ htab_private.h
@@ -0,0 +1,23 @@
+// htab_private.h
+// Řešení IJC-DU2, příklad b), 12.04.2022
+// Autor: Filip Haužvic, FIT
+// Přeloženo: gcc 9.3.0
+
+#ifndef HTAB_PRIVATE_H
+#define HTAB_PRIVATE_H 1
+
+#include "htab.h"
+
+typedef struct htab_item htab_item_t;
+struct htab_item{
+    htab_pair_t item;
+    htab_item_t *next;
+};
+
+struct htab{
+    size_t size; // number of items in htab
+    size_t arr_size; // size of array
+    htab_item_t *arr_ptr[]; // pointer to dynamic array of items (actual table)
+};
+
+#endif
