84d959513b08743074fde5f8169225170e49eac7
Filip Haužvic <hauzvicfilip@gmail.com>
2022-04-16 13:10:20 +0200

    All htab functions implemented (without resizing)

--- htab_resize.c
+++ htab_resize.c
@@ -3,8 +3,23 @@
 // Autor: Filip Haužvic, FIT
 // Přeloženo: gcc 9.3.0
 
+#include <stdlib.h>
 #include "htab.h"
+#include "htab_private.h"
 
 void htab_resize(htab_t *t, size_t newn){
-    return;
+    htab_t *new_table = htab_init(newn);
+
+    for(size_t i = 0; i < t->arr_size; i++){
+        htab_item_t *tmp = t->arr_ptr[i];
+        while(tmp != NULL){
+            htab_pair_t *moved = htab_lookup_add(new_table, tmp->item.key);
+            moved->value = tmp->item.value;
+
+            tmp = tmp->next;
+        }
+    }
+
+    htab_free(t);
+    *t = *new_table;
 }
